aba069d798edf7206f4414f19a86064f8ea9dc28
sebaszeledon <sebas.zeledon@gmail.com>
2024-05-06 14:14:26 -0600

    define variables

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -7,13 +7,33 @@
  // SPDX-License-Identifier: MIT
  pragma solidity 0.8.24;
 
+ error Raffle__NotEnoughETHEntered();
+
  contract Raffle {
+    // State Variables
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
 
     function enterRaffle() {
 
+        if (msg.value < i_entranceFee) { 
+            revert Raffle__NotEnoughETHEntered(); 
+        }
     }
 
-    function pickRandomWinner() {
-        
+    // function pickRandomWinner() {
+
+    // }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
     }
  }
