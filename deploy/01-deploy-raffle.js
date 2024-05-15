d959995e81b1384bdeea73bb177bcfd42645ff9b
sebaszeledon <sebas.zeledon@gmail.com>
2024-05-14 20:09:16 -0600

    update

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,11 @@
+module.exports = async ( { getNamedAccounts, deployments } ) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts(); 
+
+    const raffle = await deploy("Raffle", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: 6
+    });
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,48 @@ require("hardhat-contract-sizer");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "oxkey";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
+
 module.exports = {
   solidity: "0.8.24",
+  namedAccounts: {
+    deployer: {
+        default: 0,
+        player: {
+          default: 1,
+        }, 
+    },
+  },
+  defaultNetwork: "hardhat",
+  solidity: {
+    compilers: [
+        {
+            version: "0.8.0",
+        },
+        {
+            version: "0.6.6",
+        },
+        {
+          version: "0.8.24",
+        },
+    ],
+  },
+  networks: {
+    hardhat: {
+        chainId: 31337,
+        blockConfirmations: 1,
+        // gasPrice: 130000000000,
+    },
+    sepolia: {
+        url: SEPOLIA_RPC_URL,
+        accounts: [PRIVATE_KEY],
+        chainId: 11155111,
+        blockConfirmations: 2,
+    },
+  },
 };
 
