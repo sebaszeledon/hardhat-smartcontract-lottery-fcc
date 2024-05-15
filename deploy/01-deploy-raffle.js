ede5480785132678de6b44ffd18107be5ba32f11
sebaszeledon <sebas.zeledon@gmail.com>
2024-05-14 21:38:58 -0600

    update

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,12 +1,26 @@
 const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async ( { getNamedAccounts, deployments } ) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts(); 
+    let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock;
 
+    if (developmentChains.includes(network.name)){
+        // create VRFV2 Subscription
+        vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait();
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        // Fund the subscription
+        // Our mock makes it so we don't actually have to worry about sending fund
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
+    }
+    const args = []
     const raffle = await deploy("Raffle", {
         from: deployer,
-        args: [],
+        args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
