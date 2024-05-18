ea1a01d3b239ca6cb5c00d45ea8e51bba73f3688
sebaszeledon <sebas.zeledon@gmail.com>
2024-05-18 11:19:05 -0600

    update

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,9 +1,12 @@
 const { network } = require("hardhat");
-const { developmentChains } = require("../helper-hardhat-config");
+const { developmentChains, networkConfig } = require("../helper-hardhat-config");
+
+const FUND_AMOUNT = ethers.parseEther("1") // 1 Ether, or 1e18 (10^18) Wei
 
 module.exports = async ( { getNamedAccounts, deployments } ) => {
     const { deploy, log } = deployments;
     const { deployer } = await getNamedAccounts(); 
+    const chainId = network.config.chainId;
     let vrfCoordinatorV2Address, subscriptionId, vrfCoordinatorV2Mock;
 
     if (developmentChains.includes(network.name)){
@@ -15,14 +18,26 @@ module.exports = async ( { getNamedAccounts, deployments } ) => {
         subscriptionId = transactionReceipt.events[0].args.subId;
         // Fund the subscription
         // Our mock makes it so we don't actually have to worry about sending fund
-        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT); //15:10:45
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT);
+    } else {
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subscriptionId = networkConfig[chainId]["subscriptionId"];
     }
-    const args = []
+
+    const entranceFee = networkConfig[chainId]["raffleEntranceFee"];
+    const args = [
+        vrfCoordinatorV2Address,
+        entranceFee,
+        networkConfig[chainId]["gasLane"],
+        subscriptionId,
+        networkConfig[chainId]["callbackGasLimit"],
+        networkConfig[chainId]["keepersUpdateInterval"],
+        //networkConfig[chainId]["raffleEntranceFee"],
+    ]
     const raffle = await deploy("Raffle", {
         from: deployer,
         args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
-    
 }
