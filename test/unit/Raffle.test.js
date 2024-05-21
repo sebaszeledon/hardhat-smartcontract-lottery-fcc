39eae526e738f666db5a6c2443eb47676996e914
sebaszeledon <sebas.zeledon@gmail.com>
2024-05-20 21:03:33 -0600

    start testing

---
+++ test/unit/Raffle.test.js
@@ -0,0 +1,4 @@
+const { assert, expect } = require("chai");
+const { network, deployments, ethers } = require("hardhat");
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config");
+
