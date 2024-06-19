19f235746258b906050a4b4658bab5882afa4dec
sebaszeledon <sebas.zeledon@gmail.com>
2024-06-18 21:01:10 -0600

    scripts updated

--- scripts/mockOffchain.js
+++ scripts/mockOffchain.js
@@ -2,12 +2,12 @@ const { ethers, network } = require("hardhat")
 
 async function mockKeepers() {
     const raffle = await ethers.getContract("Raffle")
-    const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
-    const { upkeepNeeded } = await raffle.callStatic.checkUpkeep(checkData)
+    const checkData = ethers.keccak256(ethers.toUtf8Bytes(""))
+    const { upkeepNeeded } = await raffle.checkUpkeep.staticCall(checkData)
     if (upkeepNeeded) {
         const tx = await raffle.performUpkeep(checkData)
         const txReceipt = await tx.wait(1)
-        const requestId = txReceipt.events[1].args.requestId
+        const requestId = raffle.interface.parseLog(txReceipt.logs[1]).args.requestId;
         console.log(`Performed upkeep with RequestId: ${requestId}`)
         if (network.config.chainId == 31337) {
             await mockVrf(requestId, raffle)
